0babe2522aafd2bb197eecca7ea2f100c975295f
mohamedagina <mohamedagina001@gmail.com>
2021-03-15 21:51:21 +0200

    test units

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
 import { checkUrl } from "./js/urlChecker";
-import { handleSubmit, postUrl } from "./js/formHandler";
+import { handleSubmit } from "./js/formHandler";
 import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 
-export { checkUrl, handleSubmit, postUrl };
+export { checkUrl, handleSubmit };
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,20 +1,20 @@
 const handleSubmit = async (event) => {
   event.preventDefault();
-
+  const resultDiv = document.querySelector("#result");
   // check whether url is valid or not
   const inputUrl = document.getElementById("url").value;
   if (Client.checkUrl(inputUrl)) {
-    const data = await Client.postUrl("http://localhost:8081/analyze", {
+    resultDiv.innerHTML = "Loading...";
+    const data = await postUrl("http://localhost:8081/analyze", {
       url: inputUrl,
     });
     try {
-      const resultDiv = document.querySelector("#result");
       resultDiv.innerHTML = `Model: ${data.model} <br>Score: ${data.score_tag} <br>Agreement: ${data.agreement} <br>Confidence: ${data.confidence}`;
     } catch {
       console.log("couldn`t update ui");
     }
   } else {
-    alert("Invalid URL1");
+    alert("Invalid URL");
   }
 };
 ///////////
@@ -31,4 +31,4 @@ const postUrl = async (url = "", data = {}) => {
   return result;
 };
 
-export { handleSubmit, postUrl };
+export { handleSubmit };
